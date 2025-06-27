02e23cc7fcee1e55140927121f6bfa56500fff8a
Jaykler <julyme10@hotmail.com>
2025-06-26 23:05:02 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MiSerieApp',
+  webDir: 'www'
+};
+
+export default config;
